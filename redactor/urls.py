367806448e23605a26baaf00f5baa61025501d82
Palo <pjestrada006@hotmail.com>
2019-12-06 15:46:42 -0600

    fix urls

--- redactor/urls.py
+++ redactor/urls.py
@@ -1,16 +1,15 @@
-from django.conf.urls import url
+from django.urls import path
 from redactor.views import redactor_upload
 from redactor.forms import FileForm, ImageForm
 
 
 urlpatterns =[
-    '',
-    url('^upload/image/(?P<upload_to>.*)', redactor_upload, {
+    path('upload/image/<str:upload_to>', redactor_upload, {
         'form_class': ImageForm,
         'response': lambda name, url: '<img src="%s" alt="%s" />' % (url, name),
     }, name='redactor_upload_image'),
 
-    url('^upload/file/(?P<upload_to>.*)', redactor_upload,
+    path('upload/file/<str:upload_to>', redactor_upload,
         {
         'form_class': FileForm,
         'response': lambda name, url: '<a href="%s">%s</a>'.format(url, name)},
