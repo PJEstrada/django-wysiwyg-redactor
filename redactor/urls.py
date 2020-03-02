b642672a5a054998397d3d167ac0b99655724230
Palo <pjestrada006@hotmail.com>
2020-03-02 09:55:35 -0600

    fix urls kwargs

--- redactor/urls.py
+++ redactor/urls.py
@@ -2,16 +2,17 @@ from django.urls import path
 from redactor.views import redactor_upload
 from redactor.forms import FileForm, ImageForm
 
-
-urlpatterns =[
-    path('upload/image/<str:upload_to>', redactor_upload, {
+urlpatterns = [
+    path('upload/image/', redactor_upload, {
         'form_class': ImageForm,
+        'kwargs': {'upload_to': None},
         'response': lambda name, url: '<img src="%s" alt="%s" />' % (url, name),
     }, name='redactor_upload_image'),
 
-    path('upload/file/<str:upload_to>', redactor_upload,
-        {
-        'form_class': FileForm,
-        'response': lambda name, url: '<a href="%s">%s</a>'.format(url, name)},
-        name='redactor_upload_file'),
+    path('upload/file/', redactor_upload,
+         {
+             'form_class': FileForm,
+             'kwargs': {'upload_to': None},
+             'response': lambda name, url: '<a href="%s">%s</a>'.format(url, name)},
+         name='redactor_upload_file'),
 ]
