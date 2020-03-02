2a9e2afa8bb16a018ebd813f1dc9191e38a9a857
Palo <pjestrada006@hotmail.com>
2020-03-02 10:06:20 -0600

    fixed kwargs

--- redactor/urls.py
+++ redactor/urls.py
@@ -3,16 +3,19 @@ from redactor.views import redactor_upload
 from redactor.forms import FileForm, ImageForm
 
 urlpatterns = [
-    path('upload/image/', redactor_upload, {
-        'form_class': ImageForm,
-        'kwargs': {'upload_to': None},
-        'response': lambda name, url: '<img src="%s" alt="%s" />' % (url, name),
-    }, name='redactor_upload_image'),
+    path('upload/image/', redactor_upload,
+         {
+             'form_class': ImageForm,
+             'response': lambda name, url: '<img src="%s" alt="%s" />' % (url, name),
+         },
+         kwargs={'upload_to': None},
+         name='redactor_upload_image'),
 
     path('upload/file/', redactor_upload,
          {
              'form_class': FileForm,
-             'kwargs': {'upload_to': None},
-             'response': lambda name, url: '<a href="%s">%s</a>'.format(url, name)},
+             'response': lambda name, url: '<a href="%s">%s</a>'.format(url, name)
+         },
+         kwargs={'upload_to': None},
          name='redactor_upload_file'),
 ]
